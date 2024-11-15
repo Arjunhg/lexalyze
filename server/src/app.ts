1cfeb910f11b59c5f09a1bdfbb2b3e802c191aaf
Arjun Sharma <arjunsharma03.sa@gmail.com>
2024-11-16 03:31:50 +0530

    Bind server to listen on "0.0.0.0"

--- server/src/app.ts
+++ server/src/app.ts
@@ -78,7 +78,7 @@ app.use("/contracts", contractsRoute);
 app.use("/payment", paymentRoute);
 
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 4000;
+app.listen(PORT, "0.0.0.0",() => {
     console.log(`Server running on port ${PORT}`);
 });
